5a5e857830586ceb32ddf3c9a23f6268daed6209
Alvaro Padilla <alvaro.padilla@bitsamericas.com>
2022-11-26 12:16:35 -0500

    Agregando primer commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,56 @@
+const { io } = require('../server');
+const Usuarios = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+
+const usuarios = new Usuarios();
+
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (data, callback) => {
+
+        if (!data.nombre || !data.sala) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre/sala es necesario'
+            });
+        }
+
+        client.join(data.sala);
+
+        usuarios.agregarPersona(client.id, data.nombre, data.sala);
+
+        // Actualiza a todos los usuarios en todos los clientes que estan en la misma sala
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+
+        // Envia un mensaje a todos los usuarios en todos los clientes de una misma sala
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${ data.nombre } se unió`));
+
+        // Actualiza todos los usuarios del un solo cliente
+        callback(usuarios.getPersonasPorSala(data.sala));
+    });
+
+    client.on('crearMensaje', (data, callback) => {
+
+        let persona = usuarios.getPersona(client.id);
+
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+        callback(mensaje);
+    });
+
+    client.on('disconnect', () => {
+        let personaBorrada = usuarios.borrarPersona(client.id);
+
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administradro', `${personaBorrada.nombre} salió`));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
+    });
+
+    // Mensajes privados
+    client.on('mensajePrivado', (data) => {
+        let persona = usuarios.getPersona(client.id);
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+    });
+});
+
